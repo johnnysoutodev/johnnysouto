a6aebafd48989901d44813e7a3a85fc63735a405
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-06-27 17:03:42 -0300

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -12,6 +12,11 @@ module.exports = function(grunt){
             js: {
                 src: ['src/www/current/js/jquery.js', 'src/www/current/js/main.js'],
                 dest: 'dist/www/js/scripts.min.js'
+            },
+
+            css: {
+                src: ['src/www/current/css/*.css'],
+                dest: 'src/www/current/style.css'
             }
         }
 
