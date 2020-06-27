7045f7dafabdcc18e2c1276969176091155ce1aa
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-06-27 16:59:08 -0300

    Create gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt){
+
+    // Config
+    grunt.initConfig({
+
+        clearn: {
+            src: ['dist/www/**/*'],
+            filter: 'isFile'
+        },
+
+        concat: {
+            js: {
+                src: ['src/www/current/js/jquery.js', 'src/www/current/js/main.js'],
+                dest: 'dist/www/js/scripts.min.js'
+            }
+        }
+
+    });
+}
