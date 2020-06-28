557803aee666ef0e9b8ee4e13efd08b0294cbba7
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-06-28 17:54:33 -0300

    Update gruntfile.js
    ajustando/adicionando as tarefas de clean

--- gruntfile.js
+++ gruntfile.js
@@ -3,9 +3,26 @@ module.exports = function(grunt){
     // Config
     grunt.initConfig({
 
-        clearn: {
-            src: ['dist/www/**/*'],
-            filter: 'isFile'
+        clean: {
+            dist:{
+                src: ['dist/www/**/*']
+            },
+
+            tmp: {
+                src: ['.tmp/**/*']
+            },
+
+            index: {
+                src: ['dist/www/index.html']
+            },
+
+            imagesTmp: {
+                src: ['.tmp/images/**/*']
+            },
+
+            images: {
+                src: ['dist/app/images/**/*']
+            }
         },
 
         concat: {
@@ -32,6 +49,34 @@ module.exports = function(grunt){
                 expand: false,
                 src: 'src/www/*.html',
                 dest: 'dist/www/*.html'
+            }
+        },
+
+        cssmin: {
+            target: {
+                files: {
+                    'dist/www/css/style.min.css': 'dist/www/css/css/style.min.css'
+                }
+            }
+        },
+
+        uglify: {
+            dist: {
+                files: [{
+                    src: 'dist/www/js/scripts.min.js',
+                    dest: 'dist/www/js/scripts.min.js'
+                }]
+            }
+        },
+
+        image: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/www/current/images',
+                    src: ['**/*.{png,jpg,gif,svg'],
+                    dest: 'dist/www/images'
+                }]
             }
         }
 
