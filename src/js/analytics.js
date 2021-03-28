c2910ecca7d6a85dbdfc35f92b5a65b7ed96fe66
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-03-27 21:05:52 -0300

    Update analytics.js
    adicionando o google analytics

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,1 +1,11 @@
 // analytics
+
+// Google Analytics
+
+function activeGA() {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-05QDHZNLE0');
+}
