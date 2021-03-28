9e0aec9f916fc3dcc88079fbfda7f25a9adbfea7
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-03-28 01:21:26 -0300

    Update analytics.js
    melhorando a função gtag

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -6,12 +6,16 @@ function refuseGA(){
     window['ga-disable-G-05QDHZNLE0'] = true;
 }
 
-
 function acceptGA() {
     window['ga-disable-G-05QDHZNLE0'] = false;
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-05QDHZNLE0');
+    gtag('config', 'G-05QDHZNLE0', {
+        'cookie_prefix': 'JSDTech',
+        'cookie_domain': 'www.jsdeveloper.com.br',
+        'cookie_expires': 0 * 24 * 60 * 60
+    });
+
 }
