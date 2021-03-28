475da86e5bef0f32bb9ccac451c2ab8f163df9f9
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-03-28 02:52:41 -0300

    Update analytics.js

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -7,15 +7,16 @@ function refuseGA(){
 }
 
 function acceptGA() {
+    var exdays = 30;
     window['ga-disable-G-05QDHZNLE0'] = false;
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
     gtag('config', 'G-05QDHZNLE0', {
-        'cookie_prefix': 'JSDTech',
-        'cookie_domain': 'www.jsdeveloper.com.br',
-        'cookie_expires': 0 * 24 * 60 * 60
+        'cookie_domain': 'jsdeveloper.com.br',
+        'cookie_expires': exdays * 24 * 60 * 60,
+        'cookie_update': false
     });
 
 }
