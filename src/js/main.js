83a65fdc98ca5d62dcf7072ac349c71a23b1009d
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2021-03-27 21:06:01 -0300

    Update main.js
    desvinculando o google analytics

--- src/js/main.js
+++ src/js/main.js
@@ -49,14 +49,4 @@ function checkCookie(){
     return showCookieAlert();
 }
 
-// Google Analytics
-
-function activeGA() {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-05QDHZNLE0');
-}
-
 checkCookie();
